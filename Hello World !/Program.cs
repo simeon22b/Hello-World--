4b798c186f8d714f08e61caf96a91c2abbef088d
Simeon <simeon22b@gmail.com>
2015-04-28 11:34:21 +0300

    Hello World !

---
+++ Hello World !/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is my first program !");
+
+        }
+
+    }
+}
